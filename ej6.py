3475b74c38d076eb94656e7d16809f7bc54421c1
valuvilla <valuvp@hotmail.com>
2022-12-19 10:03:06 +0100

    ej

--- ej6.py
+++ ej6.py
@@ -17,8 +17,17 @@ def area_parche(diametro, porcentaje):
     area_parche = area_circulo(diametro) * porcentaje
     return area_parche
 
-def circunferencia(diametro):
+def circunferencia(diametro, porcentaje):
+    area_parche(diametro, porcentaje)
     circunferencia = pi * diametro
+    #aproximar circinferencia
+    circunferencia = round(circunferencia, 0)
     return circunferencia
 
+#probamos el codigo
+print("El area del parche circular es: ", area_parche(10, 0.5))
 
+
+if __name__=='__main__':
+    print("El area del parche circular es: ", area_parche(10, 0.5))
+    print("La circunferencia del parche circular es: ", circunferencia(10, 0.5))
