4971ba0e62559c27d1dcba9d7fbc0b0b585cff6b
valuvilla <valuvp@hotmail.com>
2022-12-19 08:58:48 +0100

    intento

--- ejercicio5.py
+++ ejercicio5.py
@@ -4,7 +4,7 @@ def hollow_triangle ( n ): # n es el numero de filas de la piramide
         if i==1:
             print('_'*(n-1)+'#'*1+'_'*(n-1))
         elif i!=n:
-            print('_'*(n-i)+'#'*1+'_'*(i-1)+'#'*1+'_'*(n-i))
+            print('_'*(n-2)+'#'*1+'_'*(2**(i-2))+'#'*1+'_'*(n-2))
         else:
             print('#'*(2*n-1))
 
