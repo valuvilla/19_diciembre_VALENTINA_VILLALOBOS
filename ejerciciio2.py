598e57e4c42c2b4dbf750b8a7bb3ea69803b3b6e
valuvilla <valuvp@hotmail.com>
2022-12-19 09:22:55 +0100

    eje3

--- ejerciciio2.py
+++ ejerciciio2.py
@@ -1,1 +1,15 @@
-pt
+from sympy import sympify, solve, Symbol, pprint
+
+from string import ascii_lowercase
+
+sym1=ascii_lowercase.replace('','')
+sym2=sym1.split()
+
+sym2=Symbol('sym1')
+
+def ecuacion():
+    ecuacion = int(input('Introduzca la ecuacion: '))
+    ecuacion.replace('','*')
+    return sympify(ecuacion)
+
+print(ecuacion())
--- ejercicio5.py
+++ ejercicio5.py
@@ -1,11 +1,13 @@
 
 def hollow_triangle ( n ): # n es el numero de filas de la piramide
     for i in range(1,n+1):
-        if i==1:
-            print('_'*(n-1)+'#'*1+'_'*(n-1))
-        elif i!=n:
-            print('_'*(n-2)+'#'*1+'_'*(2**(i-2))+'#'*1+'_'*(n-2))
+        if i!=n and i!=1:
+            print('_'*(n-i)+'#'+'_'*(2*(i-2)+1)+'#'+'_'*(n-i))
+        elif i==1:
+            print('_'*(n-1)+'#'+'_'*(n-1))
         else:
-            print('#'*(2*n-1))
+            print('#'*(2*i-1))
 
 hollow_triangle(5)
+
+ #'_____#_____', '____#_#____', '___#___#___', '__#_____#__', '_#_______#_', '###########']
