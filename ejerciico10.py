c043833c601cb89b4fc2f0f6fce6e0a5dd43512e
valuvilla <valuvp@hotmail.com>
2022-12-19 09:35:08 +0100

    commit

---
+++ ejerciico10.py
@@ -0,0 +1,13 @@
+
+def Tribonnaci(*args, n):
+    for i in range(n):
+         if n == 1:
+            return args[0]
+        elif n == 2:
+            return args[1]
+        elif n == 3:
+            return args[2]
+        else:
+            args[i]=Tribonnaci(*args, n=i-1) + Tribonnaci(*args, n=i-2) + Tribonnaci(*args, n=n-3)
+
+print(Tribonnaci(1,2,3, n=5))
