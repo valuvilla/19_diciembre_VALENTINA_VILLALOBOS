984c84e93fee799ff2a97bea1e58d58e53321ff0
valuvilla <valuvp@hotmail.com>
2022-12-19 10:54:42 +0100

    gf

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,11 +1,13 @@
 #escribir numero como letra
 
-def ejercicio3():
-    n=(input("numero:"))
-    print(len(n))
-
-    
-        
+def numberOfLetter(n):
+    n='30'
+    n=n.split()
+    for i in n:
+        if i in '0123456789':
+            i=chr(int(i)+96)
     return n
+
+                
 
 print(ejercicio3())
