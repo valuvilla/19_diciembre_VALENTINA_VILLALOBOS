379b923145d164235beb19677ab5320dd65431bd
valuvilla <valuvp@hotmail.com>
2022-12-19 10:35:30 +0100

    ej

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,11 +1,3 @@
-import word2number as n2w
+#escribir numero como letra
 
-print(n2w.word_to_num("one hundred and twenty three"))
-
-# 123
-
-import word2number as n2w
-
-print(n2w.word_to_num("one hundred and twenty three"))
-
-# 123
+def 
