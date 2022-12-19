a41b31f5088a2f59266e27b0df6a0ec7202fc344
valuvilla <valuvp@hotmail.com>
2022-12-19 09:36:03 +0100

    ej

--- ejerciico10.py
+++ ejerciico10.py
@@ -1,13 +1,14 @@
 
 def Tribonnaci(*args, n):
     for i in range(n):
-         if n == 1:
+        if n == 1:
             return args[0]
         elif n == 2:
             return args[1]
         elif n == 3:
             return args[2]
         else:
-            args[i]=Tribonnaci(*args, n=i-1) + Tribonnaci(*args, n=i-2) + Tribonnaci(*args, n=n-3)
+            args[i]=Tribonnaci(*args, n=i-1) + Tribonnaci(*args, n=i-2) + Tribonnaci(*args, n=i-3)
+    return args
 
 print(Tribonnaci(1,2,3, n=5))
